8213a8d87affff0c9ee2e33091993fd945d4eae2
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2023-12-07 14:48:48 +0200

    first commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const user = useSelector((data) => data.auth);
   return (
     // just for test
     <>
-      <div>{user.name !== "" ? `hello..! ${user.name} ` : "pleas login"}</div>
+      <div>{user.name !== "" ? `hello..! ${user.name} ` : <>please <Link to={`/login`}>login</Link></>}</div>
     </>
   );
 };
